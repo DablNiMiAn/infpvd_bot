d1b866b8341f45e6426883c48fc19b8c7590141b
DablNiMiAn <166307014+DablNiMiAn@users.noreply.github.com>
2026-03-08 14:39:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-BOT_TOKEN = "8274876194:AAFsjAFlFBZ4eqvvyioq9U611T9N4mp8NP0"  # Замените на токен вашего бота
-ADMIN_ID = 1179743704  # Замените на ваш Telegram ID
+BOT_TOKEN = ""  # Замените на токен вашего бота
+
+ADMIN_ID = 1  # Замените на ваш Telegram ID
